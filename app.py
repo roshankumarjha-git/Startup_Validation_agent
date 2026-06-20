4b5ca92abbe309715b45624aa4a835110d5ee113
roshankumarjha-git <roshankumarjha2007@gmail.com>
2026-06-20 09:09:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,8 +115,7 @@ body {
 .roshan-badge:hover {
     transform: scale(1.05);
 
-    box-shadow:
-        0 0 12px rgba(239,68,68,0.20);
+    box-shadow: none !important;
 }
 
 .startup-class textarea {
@@ -271,6 +270,7 @@ with gr.Blocks(
                     container=False,
                     elem_classes="output-class",
                     label="📊 Validation Report",
+                    placeholder="Your validation report will appear here after analysis"
                     lines=10
                 )
 
